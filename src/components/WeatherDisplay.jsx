714b5851d2c0ba2fdb97c3ceced68c977089fe33
CiarJ <cjones.de117@gmail.com>
2024-01-22 14:42:16 +0100

    Added CSS styling for weather cards

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import './WeatherDisplay.css';
 
 const WeatherDisplay = (props) => {
   const weather = props.weather;
@@ -9,20 +10,35 @@ const WeatherDisplay = (props) => {
   console.log(day);
 
   return (
-    <div>
-      <h1>Today is {props.day.date}</h1>
-      <h1>Today's weather is {day.day.condition.text}</h1>
+    <div className="weatherCard">
+      <h1>{props.day.date}</h1>
       <img src={day.day.condition.icon} alt="" />
+      <h1>{day.day.condition.text}</h1>
 
-      <h1>The temperature today is {locationTemp}°C</h1>
-      <div>
-        <p>Max: {day.day.maxtemp_c}°C</p>
-        <p>Min: {day.day.mintemp_c}°C</p>
-        <p>Avg: {day.day.avgtemp_c}°C</p>
-      </div>
-      <div>
-        <p>Rain Chance : {day.day.daily_chance_of_rain}</p>
-        <p>Rain Chance : {day.day.daily_chance_of_snow}</p>
+      <div className="tempDiv">
+        <div className="tableValueRow">
+            <p>Max:</p>
+            <p>{day.day.maxtemp_c}°C</p>
+        </div>
+        <div className="tableValueRow">
+            <p>Min:</p>
+            <p>{day.day.mintemp_c}°C</p>
+        </div>
+        <div className="tableValueRow">
+            <p>Avg:</p>
+            <p>{day.day.avgtemp_c}°C</p>
+        </div>
+    </div>
+
+      <div className="rainDiv">
+        <div className="tableValueRow">
+            <p>Rain Chance : </p>
+            <p>{day.day.daily_chance_of_rain}</p>
+        </div>
+        <div className="tableValueRow">
+            <p>Rain Chance : </p>
+            <p>{day.day.daily_chance_of_snow}</p>
+        </div>
       </div>
     </div>
   );
