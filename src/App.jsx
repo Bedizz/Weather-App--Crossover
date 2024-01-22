fc0efb0a032d347c6a2c74806fb95b8c742515a0
Tonio <tonio.suessdorf@gmail.com>
2024-01-22 11:00:31 +0100

    Add text input functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 import SearchBar from "./components/SearchBar";
 
 function App() {
+  const updateSearch = (searchInput) => {
+    console.log(searchInput);
+  };
+
   return (
     <>
       <div className="bg-neutral-900 h-screen">
-        <SearchBar />
+        <SearchBar updateSearch={updateSearch} />
       </div>
     </>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-const SearchBar = () => {
+const SearchBar = (updateSearch) => {
+  const handleInput = (e) => {
+    this.props.updateSearch(e.target.value);
+  };
+
   return (
     <div className="w-full flex justify-center">
       <input
         type="text"
-        className="bg-neutral-300 mt-20 p-2 w-4/5 sm:w-80 h-10 rounded focus:outline-none "
+        className="bg-neutral-300 mt-20 p-2 w-4/5 sm:w-80 h-10 rounded focus:outline-none"
         placeholder="Search..."
+        onChange={handleInput}
       ></input>
     </div>
   );
