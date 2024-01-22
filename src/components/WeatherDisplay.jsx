361fbe98cddde29b415de9fb9891df65460dca44
Bedizz <turkyilmaz.bediz@gmail.com>
2024-01-22 13:28:16 +0100

    mapping and infos are done

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -1,25 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const WeatherDisplay = (props) => {
+  const weather = props.weather;
 
-    const weather = props.weather;
+  const userLocation = props.location;
+  const locationTemp = props.temp;
+  const day = props.day;
+  console.log(day);
 
-    const userLocation = props.location;
-    const locationTemp = props.temp;
-    const day = props.day;
-    console.log(day);
+  return (
+    <div>
+      <h1>Today is {props.day.date}</h1>
+      <h1>Today's weather is {day.day.condition.text}</h1>
+      <img src={day.day.condition.icon} alt="" />
 
-    
-    return (
-        <div>
-            <h1>Today is {props.day.date}</h1>
-            <h1>Today's weather is {day.day.condition.text}</h1>
-            <img src={day.day.condition.icon} alt="" />
-        
-            <h1>The temperature today is {locationTemp}°C</h1>
-        </div>
-    );
-
+      <h1>The temperature today is {locationTemp}°C</h1>
+      <div>
+        <p>Max: {day.day.maxtemp_c}°C</p>
+        <p>Min: {day.day.mintemp_c}°C</p>
+        <p>Avg: {day.day.avgtemp_c}°C</p>
+      </div>
+      <div>
+        <p>Rain Chance : {day.day.daily_chance_of_rain}</p>
+        <p>Rain Chance : {day.day.daily_chance_of_snow}</p>
+      </div>
+    </div>
+  );
 };
 
 export default WeatherDisplay;
