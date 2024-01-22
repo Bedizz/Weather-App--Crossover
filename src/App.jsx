71abfc3c65d8de8cbee3c6970cab2834b484c94c
CiarJ <cjones.de117@gmail.com>
2024-01-22 11:29:26 +0100

    First Weather API call completed for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import SearchBar from "./components/SearchBar";
+import WeatherDisplay from "./components/WeatherDisplay";
 
 function App() {
   return (
     <>
       <div className="bg-neutral-900 h-screen">
         <SearchBar />
+        <WeatherDisplay />
       </div>
     </>
   );
