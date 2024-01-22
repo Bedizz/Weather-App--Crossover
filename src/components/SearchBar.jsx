2381b5f3317cc9974f8d838d032ea0063a18c55e
Tonio <tonio.suessdorf@gmail.com>
2024-01-22 10:51:14 +0100

    Change search bar styling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const SearchBar = () => {
   return (
-    <div className="bg-neutral-900">
+    <div className="w-full flex justify-center">
       <input
         type="text"
-        className="bg-neutral-300 rounded"
+        className="bg-neutral-300 mt-20 p-2 w-4/5 sm:w-80 h-10 rounded focus:outline-none "
         placeholder="Search..."
       ></input>
     </div>
